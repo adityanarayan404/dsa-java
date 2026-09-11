ab8bf2598b1e19c6ff74c9aa0d9cf033b28f25df
ADITYA NARAYAN <connect.aditya.in@gmail.com>
2026-09-11 18:05:22 +0530

    Time: 2 ms (99.53%) | Memory: 45.5 MB (21.70%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int [] ans = new int [nums1.length];
+        int[] nextGreater = new int[10001];
+        Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0 ; i < nums1.length; i++){
-           int target = nums1[i];
-           ans[i] = -1;
-
-           for(int j = 0; j<nums2.length; j++){
-            if(nums2[j] == target){
-
-                // look for next greater element
-                for(int k = j+1;k<nums2.length;k++){
-                    if(nums2[k]>target){
-                        ans[i] = nums2[k];
-                        break;
-
-                    }
-            
-                }
-                break;
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
+                stack.pop();
             }
-
-           }
+            nextGreater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums2[i]);
         }
-        return ans;
+
+        for (int i = 0; i < nums1.length; i++) {
+            nums1[i] = nextGreater[nums1[i]];
+        }
+
+        return nums1;
     }
-
-
 }
