39508e6e59d26c89a077ffb9e60fe2a52ab78623
ADITYA NARAYAN <connect.aditya.in@gmail.com>
2026-09-19 15:34:46 +0530

    Time: 7 ms (37.08%) | Memory: 44.7 MB (99.22%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,31 +1,51 @@
 class Solution {
-    public int evalRPN(String[] tokens) {
-        List<String> list = new ArrayList<>(Arrays.asList(tokens));
+    public int evalRPN(String[] tokens) 
+//         List<String> list = new ArrayList<>(Arrays.asList(tokens));
 
-        while(list.size()>1){
-            for (int i=0; i<list.size(); i++){
-                String t = list.get(i);
+//         while(list.size()>1){
+//             for (int i=0; i<list.size(); i++){
+//                 String t = list.get(i);
                 
-                if(t.equals("+")||t.equals("-")||t.equals("*")||t.equals("/") ) {
-                    int a = Integer.parseInt(list.get(i-2)); 
-                     int b = Integer.parseInt(list.get(i-1));
-                     int val = 0;
+//                 if(t.equals("+")||t.equals("-")||t.equals("*")||t.equals("/") ) {
+//                     int a = Integer.parseInt(list.get(i-2)); 
+//                      int b = Integer.parseInt(list.get(i-1));
+//                      int val = 0;
 
-                     if (t.equals("+")) val = a+b;
-                     else if (t.equals("-")) val = a-b;
-                     else if (t.equals("*")) val = a*b;
-                     else val = a/b;
+//                      if (t.equals("+")) val = a+b;
+//                      else if (t.equals("-")) val = a-b;
+//                      else if (t.equals("*")) val = a*b;
+//                      else val = a/b;
 
-                     list.set(i-2, String.valueOf(val));
-                     list.remove(i);
-                     list.remove(i-1);
+//                      list.set(i-2, String.valueOf(val)); //saving the value at i-2
+//                      list.remove(i);
+//                      list.remove(i-1);
 
-                     break;
+//                      break;
 
-                }
-            }
-        }
+//                 }
+//             }
+//         }
         
-        return Integer.parseInt(list.get(0));
+//         return Integer.parseInt(list.get(0));
+//     }
+// }
+{
+    Stack<Integer> st = new Stack<>();
+
+    for(String t : tokens){
+         if(t.equals("+")||t.equals("-")||t.equals("*")||t.equals("/") ){
+            int b = st.pop();
+            int a = st.pop();
+
+              if (t.equals("+")) st.push (a+b);
+                     else if (t.equals("-")) st.push (a-b);
+                     else if (t.equals("*")) st.push (a*b);
+                     else st.push (a/b);
+         } else {
+            st.push(Integer.parseInt(t));
+
+         }
     }
+    return st.peek();
+}
 }
