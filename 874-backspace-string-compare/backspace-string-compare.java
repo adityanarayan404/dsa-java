ef09e46ad3f9dfa3e955214004f3f8540774b4d9
ADITYA NARAYAN <connect.aditya.in@gmail.com>
2026-09-20 12:08:08 +0530

    Time: 3 ms (20.39%) | Memory: 43.1 MB (39.80%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return build(s).equals(build(t));
+    }
+    private String build(String str){
+        Stack<Character> st = new Stack<>();
+
+        for (char ch: str.toCharArray()){
+            if(ch == '#'){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+         for (char c: st){
+            result.append(c);
+         }
+         return result.toString();
+    }
+}
