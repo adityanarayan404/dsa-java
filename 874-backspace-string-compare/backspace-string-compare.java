b74b87320eb3026d8be1094a518589949e11ad45
ADITYA NARAYAN <connect.aditya.in@gmail.com>
2026-09-21 21:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (97.74%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,23 +1,64 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        return build(s).equals(build(t));
-    }
-    private String build(String str){
-        Stack<Character> st = new Stack<>();
+    public boolean backspaceCompare(String s, String t) //{
+//         return build(s).equals(build(t));
+//     }
+//     private String build(String str){
+//         Stack<Character> st = new Stack<>();
 
-        for (char ch: str.toCharArray()){
-            if(ch == '#'){
-                if(!st.isEmpty()){
-                    st.pop();
-                }
-            } else {
-                st.push(ch);
+//         for (char ch: str.toCharArray()){
+//             if(ch == '#'){
+//                 if(!st.isEmpty()){
+//                     st.pop();
+//                 }
+//             } else {
+//                 st.push(ch);
+//             }
+//         }
+//         StringBuilder result = new StringBuilder();
+//          for (char c: st){
+//             result.append(c);
+//          }
+//          return result.toString();
+//     }
+// }
+{
+    int i = s.length()-1;
+    int j = t.length()-1;
+
+    int skipS=0 , skipT = 0;
+
+    while(i>=0 || j>=0){
+        while(i>=0){
+            if(s.charAt(i) == '#'){
+                skipS++;  //tackle if there are multiple number of hashtags
+                i--;
+            } else if (skipS>0){
+                skipS--;
+                i--;
+            }else{
+                break;
             }
         }
-        StringBuilder result = new StringBuilder();
-         for (char c: st){
-            result.append(c);
-         }
-         return result.toString();
+        while(j>=0){
+            if(t.charAt(j) == '#'){
+                skipT++;  //tackle if there are multiple number of hashtags
+                j--;
+            } else if (skipT>0){
+                skipT--;
+                j--;
+    }else {
+        break;
     }
+}
+
+if( i>=0 && j>=0){
+    if(s.charAt(i) != t.charAt(j)) return false; 
+} else {
+    if(i>=0 || j>=0) return false;
+}
+i--;
+j--;
+    }
+    return true;
+}
 }
